1882c6997f717535b9e2c0567a5fcd8b58e3d36b
Franz A. Heinsen <franz@glassroom.com>
2019-10-14 17:31:04 -0400

    Fix typos in module doc.

--- heinsen_routing.py
+++ heinsen_routing.py
@@ -29,9 +29,11 @@ class Routing(nn.Module):
         sig2_out: [..., n_out, d_spc, d_out] variances of shape d_spc x d_out
 
     Sample usage:
-        >>> x = torch.rand(100, 4, 4)  # 100 capsules of shape 4 x 4
+        >>> a_inp = torch.rand(100)  # 100 input scores
+        >>> mu_inp = torch.rand(100, 4, 4)  # 100 capsules of shape 4 x 4
         >>> m = Routing(d_spc=4, d_out=4, n_out=10, d_inp=4, n_inp=100)
-        >>> h = m(x)  # 10 capsules of shape 4 x 4
+        >>> a_out, mu_out, sig2_out = m(x)
+        >>> print(mu_out)  # 10 capsules of shape 4 x 4
     """
     def __init__(self, d_spc, d_out, n_out, d_inp, n_inp=-1, n_iters=3, eps=1e-5):
         super().__init__()
