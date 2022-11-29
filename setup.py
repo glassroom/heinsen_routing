cd05531c0cf0a0468d88c11361e8f8d1b0a61aa6
Franz A. Heinsen <franz@glassroom.com>
2022-11-29 11:24:25 -0500

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='heinsen_routing',
-    version='1.0.2',
+    version='1.0.3',
     description='Implementation of the routing algorithm proposed by Franz A. Heinsen, 2019 and 2022 variants.',
     url='https://github.com/glassroom/heinsen_routing',
     author='Franz A. Heinsen',
